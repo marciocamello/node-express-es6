1de2b5cbea0f63cbedfd3c4373e85628a69aa8bb
marciocamello@outlook.com <netunia234>
2018-01-30 22:45:27 -0200

    Change express to restify

--- src/index.js
+++ src/index.js
@@ -5,17 +5,26 @@ class Server {
     
     constructor() {
 
+        // set port by process and default 3000
         let port = process.env.PORT || 3000;
 
         // define restify serve
-        this.server = restify.createServer();
+        this.server = restify.createServer({
+            name: 'node-es6',
+            version: '1.0.0'
+        });
+
+        // load plugins
+        this.server.use(restify.plugins.acceptParser(this.server.acceptable));
+        this.server.use(restify.plugins.queryParser());
+        this.server.use(restify.plugins.bodyParser());
 
         // load all routes
         this.router = new Router(this.server);
 
         // listen port and start server
         this.server.listen(port, () =>{
-            console.log(`Started on port ${port}`);
+            console.log(`Started on port ${port} to application ${this.server.name} from url ${this.server.url}`);
         });
     }
 }
